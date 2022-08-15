9c80ffd25bf947a48c11e76d4e634ab04012d9b8
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-08-15 11:49:13 -0300

    chore: heroku connection

--- src/Context/Auth.jsx
+++ src/Context/Auth.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-  const URL = "http://localhost:5000";
+  const URL = "https://approved-back.herokuapp.com";
 
   const [user, setUser] = useState({});
 
